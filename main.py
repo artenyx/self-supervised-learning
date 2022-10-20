abd4fb6686044bbaf1a810b74fb693c3f95ac6dd
Gerald White <geraldkwhite10@gmail.com>
2022-10-20 15:45:58 -0500

    main change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ColabExport.TrainModel import experiments, networks
 
 print("========Running network 1========")
-model_type = networks.USL_Conv6_CIFAR2
+model_type = networks.USL_Conv6_CIFAR1
 experiments.print_model_architecture(model_type)
 experiments.ssl_experiment1(model_type)
 print("\n========Running network 2========")
