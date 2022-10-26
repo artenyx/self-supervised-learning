fcb505c5d8b7379b5814608e376aa9592b95f6da
Gerald White <geraldkwhite10@gmail.com>
2022-10-26 10:15:08 -0500

    plots

--- ColabExport/plots.py
+++ ColabExport/plots.py
@@ -62,14 +62,6 @@ def emb_loader_to_array(load_path, config=None, get_loader_from_config=False):
     return emb_dataset_array, emb_target_array
 
 
-config1 = exp_config.get_exp_config()
-config1 = exp_config.reset_config_paths_colab(config1)
-data_arrays = emb_loader_to_array(config1['data_save_path']+"_embloaders_AE-S-D-USL_Conv6_CIFAR1.pt")
-
-
-plot_pca(config1, data_arrays[0])
-plot_tsne(config1, data_arrays[0], data_arrays[1])
-
 '''  
 config = exp_config.get_exp_config()
 config = exp_config.reset_config_paths_colab(config)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ColabExport.TrainModel import experiments, networks
+from ColabExport import exp_config, plots
 
 '''
 print("========Running network 1========")
@@ -14,7 +15,7 @@ print("========Running SimCLR network 1========")
 model_type = networks.USL_Conv6_CIFAR1
 experiments.print_model_architecture(model_type)
 experiments.ssl_experiment2(model_type)
-'''
+
 print("========Running AE Single Denoising network 1========")
 model_type = networks.USL_Conv6_CIFAR1
 experiments.print_model_architecture(model_type)
@@ -24,5 +25,14 @@ print("========Running AE Single Denoising network 1========")
 model_type = networks.USL_Conv6_CIFAR1
 experiments.print_model_architecture(model_type)
 experiments.ssl_experiment5(model_type)
+
+'''
+config1 = exp_config.get_exp_config()
+config1 = exp_config.reset_config_paths_colab(config1)
+data_arrays = plots.emb_loader_to_array(config1['data_save_path']+"_embloaders_AE-S-D-USL_Conv6_CIFAR1.pt")
+
+
+plots.plot_pca(config1, data_arrays[0])
+plots.plot_tsne(config1, data_arrays[0], data_arrays[1])
 
 
