3723271a212123012cea1a1132cc143b115dd44b
Gerald White <geraldkwhite10@gmail.com>
2022-10-26 12:36:16 -0500

    plots

--- main.py
+++ main.py
@@ -8,28 +8,12 @@ print("========Running network 1========")
 model_type = networks.USL_Conv6_CIFAR1
 experiments.print_model_architecture(model_type)
 experiments.ssl_experiment1(model_type)
-print("\n========Running network 2========")
-model_type = networks.USL_Conv6_CIFAR2
-experiments.print_model_architecture(model_type)
-experiments.ssl_experiment1(model_type)
-
-print("========Running SimCLR network 1========")
-model_type = networks.USL_Conv6_CIFAR1
-experiments.print_model_architecture(model_type)
-experiments.ssl_experiment2(model_type)
-
-print("========Running AE Single Denoising network 1========")
-model_type = networks.USL_Conv6_CIFAR1
-experiments.print_model_architecture(model_type)
-experiments.ssl_experiment4(model_type)
-
-print("========Running AE Single Denoising network 1========")
-model_type = networks.USL_Conv6_CIFAR1
-experiments.print_model_architecture(model_type)
-experiments.ssl_experiment5(model_type)
+'''
 
 '''
-#plots.produce_embedding_plots(samples_to_use=1000, load_obj="_embloaders_AE-S-D-USL_Conv6_CIFAR1.pt")
+plots.produce_embedding_plots(samples_to_use=1000, load_obj="_embloaders_AE-S-D-USL_Conv6_CIFAR1.pt")
+'''
+
 config = exp_config.get_exp_config()
 exp_config.reset_config_paths_colab(config)
 
