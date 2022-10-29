19619b02333de0c023844c89991c48345453004e
Gerald White <geraldkwhite10@gmail.com>
2022-10-29 17:18:36 -0500

    removing experiment

--- testcode.py
+++ testcode.py
@@ -9,5 +9,6 @@ print(torch.cuda.current_device())
 experiments.ssl_experiment_setup(model_type=networks.USL_Conv6_CIFAR1,
                                  exp_type=("AE-S", "D", "L"),
                                  num_epochs_usl=0,
-                                 num_epochs_le=1)
+                                 num_epochs_le=1,
+                                 save_embeddings=True)
 
