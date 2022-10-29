ad2167ef1519e9c2cd4591c2949844e09a44626d
Gerald White <geraldkwhite10@gmail.com>
2022-10-29 13:03:51 -0500

    removing experiment

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ColabExport.TrainModel import experiments, networks
 import torch
 
 print("========Running Network========")
-print("Device: "+str(torch.device("cuda:0" if torch.cuda.is_available() else "cpu")))
+print(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
 experiments.ssl_experiment_setup(model_type=networks.USL_Conv6_CIFAR_Sym,
                                  exp_type=("AE-S", "D", "L"),
                                  num_epochs_usl=40,
--- testcode.py
+++ testcode.py
@@ -1,5 +1,13 @@
 import torch
+
+from ColabExport.TrainModel import experiments, networks
+
 print(torch.__version__)
 print(torch.cuda.get_arch_list())
-print(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
 print(torch.cuda.current_device())
+
+experiments.ssl_experiment_setup(model_type=networks.USL_Conv6_CIFAR1,
+                                 exp_type=("AE-S", "D", "L"),
+                                 num_epochs_usl=0,
+                                 num_epochs_le=1)
+
