f7a38601871cba4dcf799878c9e3fdb368bfccfb
Gerald White <geraldkwhite10@gmail.com>
2022-10-31 12:11:51 -0500

    minor change

--- exp_plots.py
+++ exp_plots.py
@@ -2,4 +2,4 @@ from ColabExport import exp_config, plots
 
 config = exp_config.get_exp_config()
 
-plots.produce_usl_lineval_plots(config, load_path="data")
+plots.produce_usl_lineval_plots(config, load_path="data.csv")
