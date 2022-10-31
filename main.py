0295882c4b18a96a29d2cdd22d11b15555483f9c
Gerald White <geraldkwhite10@gmail.com>
2022-10-31 17:26:45 -0500

    minor change

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ print(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
 experiments.ssl_experiment_setup(model_type=networks.USL_Conv6_CIFAR_Sym,
                                  exp_type=("AE-P", "D", "NL"),
                                  num_epochs_usl=200,
-                                 num_epochs_le=150)
+                                 num_epochs_le=150,
+                                 save_embeddings=True)
 
 '''
 config = exp_config.get_exp_config()
