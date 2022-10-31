39208764bd6d19afb7f84c64adfafb0e864d20e1
Gerald White <geraldkwhite10@gmail.com>
2022-10-31 17:21:45 -0500

    minor change

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import torch
 print("========Running Network========")
 print(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
 experiments.ssl_experiment_setup(model_type=networks.USL_Conv6_CIFAR_Sym,
-                                 exp_type=("AE-S", "D", "L"),
-                                 num_epochs_usl=40,
+                                 exp_type=("AE-P", "D", "NL"),
+                                 num_epochs_usl=200,
                                  num_epochs_le=150)
 
 '''
