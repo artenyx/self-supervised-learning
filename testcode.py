d0a42014ccc1c205408c26814484cab822493c17
Gerald White <geraldkwhite10@gmail.com>
2022-11-01 14:26:13 -0500

    minor change

--- testcode.py
+++ testcode.py
@@ -7,8 +7,7 @@ print(torch.cuda.get_arch_list())
 print(torch.cuda.current_device())
 
 experiments.ssl_experiment_setup(model_type=networks.USL_Conv6_CIFAR1,
-                                 exp_type=("AE-S", "D", "NL"),
+                                 exp_type=("AE-P", "D", "NL"),
                                  num_epochs_usl=0,
-                                 num_epochs_le=1,
-                                 save_embeddings=True)
+                                 num_epochs_le=1)
 
