521d379c6b80e6cd756987bf6c7968691945c661
Gerald White <geraldkwhite10@gmail.com>
2022-11-02 21:23:32 -0500

    minor changes

--- main.py
+++ main.py
@@ -7,10 +7,27 @@ print(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
 
 experiments.test_alpha_layerwise([0.0001, 0.001, 0.01, 0.1, 0.0, 0.1, 1.0, 10])
 
-'''
 experiments.ssl_experiment_setup(model_type=networks.USL_Conv6_CIFAR_Sym,
-                                 exp_type=("AE-P", "D", "NL"),
+                                 exp_type=("AE-S", "D", "NL"),
                                  num_epochs_usl=200,
                                  num_epochs_le=150,
                                  save_embeddings=True)
-'''
+
+experiments.ssl_experiment_setup(model_type=networks.USL_Conv6_CIFAR_Sym,
+                                 exp_type=("AE-S", "ND", "NL"),
+                                 num_epochs_usl=200,
+                                 num_epochs_le=150,
+                                 save_embeddings=True)
+
+experiments.ssl_experiment_setup(model_type=networks.USL_Conv6_CIFAR_Sym,
+                                 exp_type=("AE-S", "D", "L"),
+                                 num_epochs_usl=200,
+                                 num_epochs_le=150,
+                                 save_embeddings=True)
+
+experiments.ssl_experiment_setup(model_type=networks.USL_Conv6_CIFAR_Sym,
+                                 exp_type=("AE-S", "ND", "L"),
+                                 num_epochs_usl=200,
+                                 num_epochs_le=150,
+                                 save_embeddings=True)
+
