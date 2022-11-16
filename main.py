d8de712ad5271ae3c523f56bea9f3941f7127f19
Gerald White <geraldkwhite10@gmail.com>
2022-11-16 11:32:39 -0600

    changing main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main(args):
     print("========Running Network========")
     print(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
     if args.ssl_type is None:
-        experiments.ssl_experiment_setup(exp_type=args.exp_type)
+        experiments.ssl_experiment_setup(exp_type=(args.usl_type, args.denoising, args.layerwise))
     else:
         if args.exp_type == "alpha":
             experiments.test_alpha_parallel([0.0001, 0.001, 0.01, 0.1, 0.0, 0.1, 1.0, 10])
@@ -22,10 +22,14 @@ def main(args):
 if __name__ == "main":
     parser = argparse.ArgumentParser()
     parser.add_argument("--exp_type", type=str, default=None, choices=["alpha", "strength", "class_from_path"])
+
     parser.add_argument("--ssl_type", type=tuple, default=None, help="What kind of experiment to run, first argument "
                                                                      "should be AE-S, AE-P, or SimCLR. Second "
                                                                      "element should be D or ND, Third should be L "
                                                                      "or NL.")
+    parser.add_argument("--usl_type", type=str, default=None, choices=["SimCLR", "AE-S", "AE-P"])
+    parser.add_argument("--denoising", type=str, default=None, choices=["D", "ND"])
+    parser.add_argument("--layerwise", type=str, default=None, choices=["L", "NL"])
     parser.add_argument("--usl_load_path", type=str, default=None)
-    args = parser.parse_args()
-    main(args)
+    exp_args = parser.parse_args()
+    main(exp_args)
