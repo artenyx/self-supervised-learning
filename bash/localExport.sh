300ea0ac6610c1d5a0c1d087360ddf0194de3de6
Gerald White <geraldkwhite10@gmail.com>
2023-02-23 12:38:58 -0500

    adding nonlinear projection layer

--- bash/localExport.sh
+++ bash/localExport.sh
@@ -5,10 +5,10 @@ TYPE=$1
 AOC_DIR='geraldkwhite@aoc.uchicago.edu:/home/geraldkwhite/SSLProject/'
 LOCAL_DIR='/Users/jerrywhite/Documents/01-UChicago/05-Thesis/01-ThesisExperiments/02-Exports'
 
-if [ "$TYPE" = 'EXPORT' ]; then
+if [ "$TYPE" = 'TO_LOCAL' ]; then
   SOURCE_DIR=AOC_DIR
   TARGET_DIR=LOCAL_DIR
-elif [ "$TYPE" = 'IMPORT' ]; then
+elif [ "$TYPE" = 'FROM_LOCAL' ]; then
   SOURCE_DIR=LOCAL_DIR
   TARGET_DIR=AOC_DIR
 else
