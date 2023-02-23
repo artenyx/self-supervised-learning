fa64c1ad7b05117e83a7e8dcc3962eeb9a95327d
Gerald White <geraldkwhite10@gmail.com>
2023-02-23 13:06:54 -0500

    adding nonlinear projection layer

--- bash/localExport.sh
+++ bash/localExport.sh
@@ -2,18 +2,19 @@
 # Export ExperimentFiles to local downloads folder
 
 TYPE=$1
-AOC_DIR='geraldkwhite@aoc.uchicago.edu:/home/geraldkwhite/SSLProject/ExperimentFiles'
+SERVER_DIR='geraldkwhite@aoc.uchicago.edu:/home/geraldkwhite/SSLProject/ExperimentFiles'
 LOCAL_DIR='/Users/jerrywhite/Documents/01-UChicago/05-Thesis/01-ThesisExperiments/02-Exports'
 
 if [ "$TYPE" = 'TO_LOCAL' ]; then
-  SOURCE_DIR=AOC_DIR
+  SOURCE_DIR=SERVER_DIR
   TARGET_DIR=LOCAL_DIR
 elif [ "$TYPE" = 'FROM_LOCAL' ]; then
   SOURCE_DIR=LOCAL_DIR
-  TARGET_DIR=AOC_DIR
+  TARGET_DIR=SERVER_DIR
 else
   echo FALSE
 fi
 
-scp -r SOURCE_DIR* TARGET_DIR
+[ "$TYPE" = 'TO_LOCAL' ] && scp -r $SERVER_DIR $LOCAL_DIR
+[ "$TYPE" = 'FROM_LOCAL' ] && scp -r $LOCAL_DIR $SERVER_DIR
 echo 'copy complete.'
