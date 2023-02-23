b82a1409c28b70019b6a9a8658a5eb3c0fb015e8
Gerald White <geraldkwhite10@gmail.com>
2023-02-23 13:18:20 -0500

    adding nonlinear projection layer

--- bash/localExport.sh
+++ bash/localExport.sh
@@ -2,7 +2,7 @@
 # Export ExperimentFiles to local downloads folder
 
 TYPE=$1
-SERVER_DIR='geraldkwhite@aoc.uchicago.edu:/home/geraldkwhite/SSLProject/ExperimentFiles'
+SERVER_DIR='geraldkwhite@aoc.uchicago.edu:/home/geraldkwhite/SSLProject/ExperimentFiles/ae_parallel-D-NL_02-14-2023_14-20-39cosreconalpha-0.001'
 LOCAL_DIR='/Users/jerrywhite/Documents/01-UChicago/05-Thesis/01-ThesisExperiments/02-Exports'
 
 STAT=1
